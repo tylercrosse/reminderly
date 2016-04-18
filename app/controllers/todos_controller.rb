7d671ea9ba5a23e3d76022f8a8a81aba0cdb7a1f
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-04-18 14:05:23 -0400

    Add create and delete features

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,23 @@ class TodosController < ApplicationController
   def show
     @todo = Todo.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @todo = Todo.create!(todo_params)
+    redirect_to todos_path
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:body, :author)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :todos, ony: [:index, :show]
+  resources :todos, ony: [:index, :show, :new, :create, :destroy]
 end
